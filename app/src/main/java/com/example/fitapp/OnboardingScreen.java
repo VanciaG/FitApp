7ed8a3ebd43ac8ed925675ea678ea871f806ba27
VanciaG <vancia.gabriela@yahoo.com>
2022-03-05 16:58:35 +0200

    added viewpager

--- app/src/main/java/com/example/fitapp/OnboardingScreen.java
+++ app/src/main/java/com/example/fitapp/OnboardingScreen.java
@@ -1,12 +1,26 @@
 package com.example.fitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class OnboardingScreen extends AppCompatActivity {
+
+    ViewPager mSlideViewPager;
+    LinearLayout mDotLayout;
+    Button loginbtn, signupbtn;
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +32,31 @@ public class OnboardingScreen extends AppCompatActivity {
 
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_onboarding_screen);
+
+
+        loginbtn = findViewById(R.id.login_btn);
+        signupbtn = findViewById(R.id.sign_btn);
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        mSlideViewPager.setAdapter(viewPagerAdapter);
     }
+
+
+    /*public void setUpindicator(int position){
+        dots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for(int i=0 ; i < dots.length ; i++){
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextColor(ContextCompat.getColor(context, R.color.white));
+        }
+    }*/
+
+
 }
