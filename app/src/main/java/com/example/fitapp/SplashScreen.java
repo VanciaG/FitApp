f82a9b0b86ff5350385522d03ab7fa7cb301d904
VanciaG <vancia.gabriela@yahoo.com>
2022-03-07 13:43:38 +0200

    splash screen

--- app/src/main/java/com/example/fitapp/SplashScreen.java
+++ app/src/main/java/com/example/fitapp/SplashScreen.java
@@ -1,15 +1,39 @@
 package com.example.fitapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if(!isFinishing()){
+                startActivity(new Intent(getApplicationContext(), OnboardingScreen.class));
+                finish();
+            }
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable,200);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
     }
 }
