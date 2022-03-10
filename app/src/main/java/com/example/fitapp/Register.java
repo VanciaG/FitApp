cc69446c0c0bf9f5b3aa453d63fb5121db4fcda4
VanciaG <vancia.gabriela@yahoo.com>
2022-03-10 15:01:55 +0200

    firebase

--- app/src/main/java/com/example/fitapp/Register.java
+++ app/src/main/java/com/example/fitapp/Register.java
@@ -11,8 +11,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
-
     private TextView login_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
