a49e085e56253aae925d329b68b160aa50a7b19b
VanciaG <vancia.gabriela@yahoo.com>
2022-03-10 18:43:38 +0200

    register user

--- app/src/main/java/com/example/fitapp/LogIn.java
+++ app/src/main/java/com/example/fitapp/LogIn.java
@@ -7,11 +7,20 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LogIn extends AppCompatActivity {
 
     private TextView signup_btn;
+    private EditText mEmail, mPassword;
+    private FirebaseAuth mAuth;
+    private Button loginBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +32,16 @@ public class LogIn extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_log_in);
 
+        loginBtn = findViewById(R.id.login_btn);
+        mEmail = findViewById(R.id.login_email);
+        mPassword = findViewById(R.id.login_pass);
         signup_btn = findViewById(R.id.signup);
+
+        mAuth=FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            finish();
+            return;
+        }
 
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +51,15 @@ public class LogIn extends AppCompatActivity {
                 finish();
             }
         });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
 
     }
 }
