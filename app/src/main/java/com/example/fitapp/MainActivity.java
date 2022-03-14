5a31875275fdf4f89d97bbcb17b5210684e66292
VanciaG <vancia.gabriela@yahoo.com>
2022-03-14 13:40:38 +0200

    added bottom navigation

--- app/src/main/java/com/example/fitapp/MainActivity.java
+++ app/src/main/java/com/example/fitapp/MainActivity.java
@@ -1,18 +1,47 @@
 package com.example.fitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
 
+import com.example.fitapp.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(1);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigator.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.profile:
+                    replaceFragment(new Profile());
+                    break;
+            }
+            return true;
+        });
 
 
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.body_container, fragment).commit();
     }
 }
